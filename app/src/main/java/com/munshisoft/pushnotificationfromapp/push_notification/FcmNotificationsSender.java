e0f375a554782f570af6b0b52d8a6754fa9470e8
Shamim Munshi <65328057+munshicomputer@users.noreply.github.com>
2023-11-20 18:10:10 +0600

    finalize push notification

---
+++ app/src/main/java/com/munshisoft/pushnotificationfromapp/push_notification/FcmNotificationsSender.java
@@ -0,0 +1,98 @@
+package com.munshisoft.pushnotificationfromapp;
+
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FcmNotificationsSender  {
+
+    String userFcmToken;
+    String title;
+    String body;
+    Activity activity;
+
+
+    private RequestQueue requestQueue;
+    private final String postUrl = "https://fcm.googleapis.com/fcm/send";
+    private final String fcmServerKey ="AAAAq-AuoTc:APA91bF7lMT9ZuvL8jilqQvXLQX6BBE0qI6tOJLjzIMK7Ggizq7g0T5UaJYvy3Ya75jMIKFwe23mNU0Cpmp-mojU_G09SLAOB0JBjPW7RfHU-YYl2Xl1W6NB6IfWB7Ww-GLL77-Xhqz3";
+
+    public FcmNotificationsSender(String userFcmToken, String title, String body, Activity mActivity) {
+        this.userFcmToken = userFcmToken;
+        this.title = title;
+        this.body = body;
+        this.activity = mActivity;
+    }
+
+    public void sendNotifications() {
+        requestQueue = Volley.newRequestQueue(activity);
+        JSONObject mainObj = new JSONObject();
+        try {
+            mainObj.put("to", userFcmToken);
+            JSONObject notiObject = new JSONObject();
+            notiObject.put("title", title);
+            notiObject.put("body", body);
+            notiObject.put("icon", R.drawable.ic_launcher_foreground); // TODO: 20/11/23 Need to Change Small Icon. 
+            mainObj.put("notification", notiObject);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, postUrl, mainObj, response -> {
+                Log.e("Response Form Volley", ""+response.toString());
+            }, error -> {
+                Log.e("Error Form Volley", ""+error.getLocalizedMessage());
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> header = new HashMap<>();
+                    header.put("content-type", "application/json");
+                    header.put("authorization", "key=" + fcmServerKey);
+                    return header;
+                }
+            };
+            requestQueue.add(request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void subscribeTopic(String topicName, Context context){
+        FirebaseMessaging.getInstance().subscribeToTopic(topicName).addOnCompleteListener(task -> {
+            String msg = "Subscribed";
+            if (!task.isSuccessful()) {
+                msg = "Subscribe failed";
+            }
+            Log.d(TAG, msg);
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(e -> {
+            Log.e("Error While subscribe a Topics", ""+e.getLocalizedMessage());
+        });
+    }
+
+    public static void unSubscribeTopic(String topicName, Context context){
+        FirebaseMessaging.getInstance().unsubscribeFromTopic(topicName).addOnCompleteListener(task -> {
+            String msg = "Un Subscribed";
+            if (!task.isSuccessful()) {
+                msg = "Un Subscribe failed";
+            }
+            Log.d(TAG, msg);
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(e -> {
+            Log.e("Error While Unsubscribe a Topics", ""+e.getLocalizedMessage());
+        });
+    }
+}
---
+++ app/src/main/java/com/munshisoft/pushnotificationfromapp/push_notification/FirebaseMessagingService.java
@@ -0,0 +1,69 @@
+package com.munshisoft.pushnotificationfromapp;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Vibrator;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+     NotificationManager notificationManager;
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        long[] pattern = {100, 300, 300, 300};
+        v.vibrate(pattern, -1);
+        int resourceImage;
+        if(remoteMessage.getNotification().getIcon() != null) {
+             resourceImage = getResources().getIdentifier(remoteMessage.getNotification().getIcon(), "drawable", getPackageName());
+        }else{
+            resourceImage = R.drawable.ic_launcher_foreground; // TODO: 20/11/23 Need to Change Small Icon.
+        }
+        Log.e("Topic From Firebase", ""+remoteMessage.getFrom());
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder.setSmallIcon(resourceImage);
+        } else {
+            builder.setSmallIcon(resourceImage);
+        }
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentTitle(remoteMessage.getNotification().getTitle());
+        builder.setContentText(remoteMessage.getNotification().getBody());
+        builder.setContentIntent(pendingIntent);
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(remoteMessage.getNotification().getBody()));
+        builder.setAutoCancel(true);
+        builder.setPriority(Notification.PRIORITY_MAX);
+
+        notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            String channelId = "default";
+            NotificationChannel channel = new NotificationChannel(
+                    channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+        }
+        notificationManager.notify(100, builder.build());
+    }
+}
+
+
